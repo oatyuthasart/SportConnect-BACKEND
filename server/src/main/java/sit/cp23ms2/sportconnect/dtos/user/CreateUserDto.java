07cd956beee8307ebfa7c91eacebc5d81fe828f0
oatyuthasart <yuthasart51@gmail.com>
2023-12-10 16:42:21 +0700

    Add /users for POST, GETbyID | Fix GET Activity will now send only categoryId

--- server/src/main/java/sit/cp23ms2/sportconnect/dtos/user/CreateUserDto.java
+++ server/src/main/java/sit/cp23ms2/sportconnect/dtos/user/CreateUserDto.java
@@ -1,14 +1,48 @@
-//package com.example.demo.dtos.user;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class CreateUserDto {
-//
-//}
+package sit.cp23ms2.sportconnect.dtos.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import sit.cp23ms2.sportconnect.enums.Gender;
+import sit.cp23ms2.sportconnect.enums.Role;
+import sit.cp23ms2.sportconnect.utils.ValueOfEnum;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.Instant;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateUserDto {
+    private Integer userId;
+    @Size(max = 20, message = "size must be within 24")
+    private String username;
+    @Size(max = 40, message = "size must be between 1 and 40")
+    @Email(message = "must be a well-formed email address")
+    @NotBlank(message = "must not be blank")
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @ValueOfEnum(enumClass = Role.class)
+    private String role;
+
+    private String profilePicture;
+
+    @Enumerated(EnumType.STRING)
+    @ValueOfEnum(enumClass = Gender.class)
+    private String gender;
+
+    private String dateOfBirth;
+    @Size(max = 10, message = "size must be within 10")
+    private String phoneNumber;
+    @Size(max = 24, message = "size must be within 24")
+    private String lineId;
+    private Instant lastLogin;
+}
